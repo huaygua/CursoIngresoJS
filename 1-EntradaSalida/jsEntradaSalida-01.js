2bc9771e4e1ee667c960b8d4640cc10516fa2d19
huaygua <mbhuaygua@gmail.com>
2021-01-30 10:04:03 -0300

    Primer cambio Huaygua

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola Mundo");
+	alert("(ಥ⌣ಥ)Primer modificacion en TP de la UTNFRA (ಥ⌣ಥ)");
 }
 
